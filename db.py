3af6b2d63c5c95fb5b763dfecec11a7196387a10
movags95 <45894549+movags95@users.noreply.github.com>
2022-01-25 19:35:13 +0000

    First commit

---
+++ db.py
@@ -0,0 +1,30 @@
+from psycopg2 import extras
+import psycopg2
+#connect to your db
+connection = psycopg2.connect(
+    host="localhost",
+    database="mohitdb",
+    user="postgres",
+    password="postgres",
+    port=5555
+)
+
+#create a cursor. the params allow for rows to be returned as a dict
+cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+
+cursor.execute("select id, name from employees")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    # print(row)
+    # print(row[0])
+    # print(row[1])
+    print(row['id'])
+    print(row['name'])
+
+#close the cursor
+cursor.close()
+
+#close the connection
+connection.close()
